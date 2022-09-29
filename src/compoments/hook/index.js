23d454de000cf2b7024632fef33254e919c45d1e
陈超寅 <chaoyinchen@caih.com>
2022-09-29 18:04:53 +0800

    dev

---
+++ src/compoments/hook/index.js
@@ -0,0 +1,30 @@
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "@compoments/appProvider/appContext";
+import { message } from "@src/index.js";
+import axios from "axios";
+
+export const useAxios = cfg => {
+    const { httpCfg } = useContext(AppContext);
+    const _axios = axios.create(httpCfg?.axiosCfg || {});
+    // 请求拦截
+    _axios.interceptors.request.use(config => {
+        if (config.method.toLocaleLowerCase() === "get") {
+            !config.params && (config.params = {});
+            config.params._t = new Date().getTime();
+        }
+        return config;
+    });
+
+    // 响应拦截
+    _axios.interceptors.response.use(
+        response => httpCfg.responseIntercept(response),
+        err => {
+            const { config } = err.response;
+            !config?.headers?.["NO-E-MSG"] && message.error(httpCfg.formatErrMsg(err));
+            httpCfg.httpOnReject?.(err);
+            return Promise.reject(err);
+        }
+    );
+
+    return _axios;
+};
