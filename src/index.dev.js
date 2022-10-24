605c36c9dbe40d601e31ddba07526a7c0701e965
陈超寅 <chaoyinchen@caih.com>
2022-10-24 15:02:29 +0800

    dev

--- src/index.dev.js
+++ src/index.dev.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+
+import * as THREE from "three";
 import style from "./1024.less";
 
 const App = () => {
@@ -40,17 +42,42 @@ const App = () => {
         }
     ];
 
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.querySelector("#three").appendChild(renderer.domElement);
+
+        const geometry = new THREE.BoxGeometry(2, 2, 2);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        function animate() {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    }, []);
+
     return (
         <div className={style.container}>
-            <div style={_style[index[2]]} className={`${style.box} ${style.box1}`}>
-                {/* <img src={require('@assets/83ccdc2c86007b662193223aae03a70c.jpeg')} alt="" srcset="" /> */}
-            </div>
-            <div style={_style[index[1]]} className={`${style.box} ${style.box2}`}>
-                {/* <img src={require('@assets/6122f0c04e232b65a446d13a2a1f4ec7.jpeg')} alt="" srcset="" /> */}
-            </div>
-            <div style={_style[index[0]]} className={`${style.box} ${style.box3}`}>
-                {/* <img src={require('@assets/814691e49e7d046a7d5e0a0b471c0e14.jpeg')} alt="" srcset="" /> */}
-            </div>
+            <div id='three'></div>
+            {/* <div style={_style[index[2]]} className={`${style.box} ${style.box1}`}></div>
+            <div style={_style[index[1]]} className={`${style.box} ${style.box2}`}></div>
+            <div style={_style[index[0]]} className={`${style.box} ${style.box3}`}></div> */}
         </div>
     );
 };
